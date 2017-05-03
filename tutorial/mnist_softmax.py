2450a863bddf0a6b3f69b694d49a13d58e251303
Asayu123 <Asayu123@users.noreply.github.com>
2017-05-03 12:24:57 +0900

    Refine code to more readable one.

--- tutorial/mnist_softmax.py
+++ tutorial/mnist_softmax.py
@@ -11,31 +11,28 @@ b = tf.Variable(tf.zeros([10]))  # Initial Bias
 
 # Define output
 y = tf.nn.softmax(tf.matmul(x, W) + b)
-
-# Temporal Data
+# Define Temporal Output placeholder
 y_ = tf.placeholder(tf.float32, [None, 10])
-
 # Define cross-entropy
 cross_entropy = tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(y), reduction_indices=[1]))
-
-# Training parameters
+# Define Training parameters
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
 
-# Initialize Data
-init = tf.global_variables_initializer()
+# Execute Operations
+with tf.Session() as session:
+    # Initialize Data
+    init = tf.global_variables_initializer()
+    session.run(init)
 
-session = tf.Session()
-session.run(init)
+    # Start Learning
+    for i in range(1000):
+        batch_xs, batch_ys = mnist.train.next_batch(100)
+        session.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
 
-# Start Learning
-for i in range(1000):
-    batch_xs, batch_ys = mnist.train.next_batch(100)
-    session.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+    # Define prediction and accuracy
+    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-# Define prediction and accuracy
-correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-
-# Calculate Accuracy and print result.
-print(session.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
+    # Calculate Accuracy and print result.
+    print(session.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels}))
 
